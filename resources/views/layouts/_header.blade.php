c37baee5d7f10c1c50348985584c24fc30083acc
Jeongum <aldlswjddma@naver.com>
2020-11-10 04:09:46 +0900

    connect with db and make some views

---
+++ resources/views/layouts/_header.blade.php
@@ -0,0 +1,30 @@
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<!-- Primary Meta Tags -->
+<title>Swipe - Mobile App One Page Bootstrap 5 Template</title>
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="title" content="Swipe - Mobile App One Page Bootstrap 5 Template">
+<meta name="author" content="Themesberg">
+<meta name="description" content="Free Mobile Application One Page Bootstrap 5 Template by Themesberg">
+<meta name="keywords" content="bootstrap, bootstrap 5, bootstrap 5 one page, bootstrap 5 mobile application, one page template, bootstrap 5 one page template, themesberg, themesberg one page, one page template bootstrap 5" />
+<link rel="canonical" href="https://themesberg.com/product/bootstrap/swipe-free-mobile-app-one-page-bootstrap-5-template">
+
+
+<!-- Favicon -->
+<link rel="apple-touch-icon" sizes="120x120" href="swipe/img/favicon/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="swipe/img/favicon/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="swipe/img/favicon/favicon-16x16.png">
+<link rel="manifest" href="swipe/img/favicon/site.webmanifest">
+<link rel="mask-icon" href="swipe/img/favicon/safari-pinned-tab.svg" color="#ffffff">
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="theme-color" content="#ffffff">
+
+<!-- Fontawesome -->
+<link type="text/css" href="@fortawesome/fontawesome-free/css/all.min.css" rel="stylesheet">
+
+<!-- Swipe CSS -->
+<link type="text/css" href="swipe/css/swipe.css" rel="stylesheet">
+
+<!-- NOTICE: You can use the _analytics.html partial to include production code specific code & trackers -->
+
+</head>
---
+++ resources/views/layouts/_scripts.blade.php
@@ -0,0 +1,14 @@
+<!-- Core -->
+<script src="https://code.jquery.com/jquery-3.5.1.min.js" ></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.5.4/umd/popper.min.js" ></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/headroom/0.12.0/headroom.min.js" ></script>
+
+<!-- Vendor JS -->
+<script type="text/javascript" src="https://unpkg.com/onscreen/dist/on-screen.umd.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/smooth-scroll/16.1.3/smooth-scroll.polyfills.min.js"></script>
+
+<script src="https://buttons.github.io/buttons.js"></script>
+
+<!-- Swipe JS -->
+<script src="swipe/js/swipe.js"></script>
